2f9d1b997bac277888bf2a117531273eb7fc0015
Tobías Corradi <corraditobias@gmail.com>
2023-01-29 22:03:42 -0300

    custom font added (#2)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
 import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
+import { useFonts } from 'expo-font'
+
 import Navigation from './src/navigation/Navigation'
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    'Uber-bold': require('./assets/fonts/UberMoveBold.otf'),
+    'Uber-medium': require('./assets/fonts/UberMoveMedium.otf')
+  })
+
+  if (!fontsLoaded) return null
+
   return (
     <NavigationContainer>
       <SafeAreaView style={{ flex: 1 }}>
